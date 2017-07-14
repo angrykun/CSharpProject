e06fe6e7b74878919b63542ce8cbe61f15302126
angrykun <2545662859@qq.com>
2017-07-14 12:09:27 +0800

    展示架构

---
+++ Ninesky/Ninesky.Web/Areas/Member/MemberAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Ninesky.Web.Areas.Member
+{
+    public class MemberAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Member";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Member_default",
+                "Member/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                new string[] { "Ninesky.Web.Areas.Member.Controllers" } //命名空间
+            );
+        }
+    }
+}
