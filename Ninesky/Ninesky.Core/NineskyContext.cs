c119ab0681d7c3af7910b9dbf27728e671dab81c
angrykun <2545662859@qq.com>
2017-07-17 13:17:21 +0800

    角色管理-完成

--- Ninesky/Ninesky.Core/NineskyContext.cs
+++ Ninesky/Ninesky.Core/NineskyContext.cs
@@ -13,7 +13,10 @@ namespace Ninesky.Core
         /// 管理员集合
         /// </summary>
         public DbSet<Administrator> Administrators { get; set; }
-
+        /// <summary>
+        /// 角色
+        /// </summary>
+        public DbSet<Role> Roles { get; set; }
         public NineskyContext() : base("DefaultConnection")
         {
             Database.SetInitializer<NineskyContext>(new CreateDatabaseIfNotExists<NineskyContext>());
